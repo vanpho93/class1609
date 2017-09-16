d549a62aa4563719cbb286a1a815e022162c6096
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-16 15:26:45 +0700

    trignalge

--- e.js
+++ e.js
@@ -21,16 +21,24 @@ class Line {
 }
 
 class Triangle {
-    constructor() {
-
+    constructor(pointA, pointB, pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
     }
 
     getPerimeter() {
-        
+        const l1 = new Line(this.pointA, this.pointB);
+        const l2 = new Line(this.pointB, this.pointC);
+        const l3 = new Line(this.pointA, this.pointC);
+        return l1.getLength() + l2.getLength() + l3.getLength();
     }
 }
 
 const pA = new Point(0, 0);
-const pB = new Point(0, 10);
-const l1 = new Line(pA, pB);
-console.log(l1.getLength());
+const pB = new Point(0, 1);
+const pC = new Point(1, 0);
+// const l1 = new Line(pA, pB);
+// console.log(l1.getLength());
+const t1 = new Triangle(pA, pB, pC);
+console.log(t1.getPerimeter());
