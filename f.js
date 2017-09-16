ee72f112f3cfb11feef5988d0f7e325642c16352
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-16 15:33:34 +0700

    show

---
+++ f.js
@@ -0,0 +1,15 @@
+// const arr = new Array();
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const numbers = [1, 3, 6, 2, 4, 9];
+
+const people = [
+    new Person('Teo Nguyen', 10),
+    new Person('Ti Nguyen', 15),
+    new Person('Tun Nguyen', 13),
+];
