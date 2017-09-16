eabe3bfa68f1bdb76600bfba0368efda09767b1c
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-16 15:44:42 +0700

    2 funcs

--- f.js
+++ f.js
@@ -13,3 +13,22 @@ const people = [
     new Person('Ti Nguyen', 15),
     new Person('Tun Nguyen', 13),
 ];
+
+function getBigArrNum(arrNumbers) {
+    const arrOutput = [];
+    for (let i = 0; i < arrNumbers.length; i++) {
+        if (arrNumbers[i] > 2) arrOutput.push(arrNumbers[i]);
+    }
+    return arrOutput;
+}
+
+function getOldPeople(arrPerson) {
+    //return mang cua cac phan tu co tuoi > 12
+    const arrOutput = [];
+    for (let i = 0; i < arrPerson.length; i++) {
+        if (arrPerson[i].age > 2) arrOutput.push(arrPerson[i]);
+    }
+    return arrOutput;
+}
+
+console.log(getBigArrNum(numbers))
